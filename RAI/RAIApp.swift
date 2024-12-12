1a5fb079c93207bf01297f1d2728a65aa99210a7
ArdiJorganxhi <ardijorganxhiu@gmail.com>
2024-12-12 22:56:30 +0100

    Initial Commit

---
+++ RAI/RAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RAIApp.swift
+//  RAI
+//
+//  Created by Ardi Jorganxhi on 12/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct RAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
